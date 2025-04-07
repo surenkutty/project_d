a47f43b67f3dd5f0abb4cb413d8c0d347a27ea77
surenkutty <surenkutty2004@gmail.com>
2025-04-07 13:38:39 +0530

    fix list cegory places

--- dindigul/views.py
+++ dindigul/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework import viewsets, filters
 from .models import Place, Category
 from .serializers import PlaceSerializer, CategorySerializer
@@ -6,6 +8,12 @@ from .serializers import PlaceSerializer, CategorySerializer
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    @action(detail=True, methods=['get'])
+    def places(self, request, pk=None):
+        category = self.get_object()
+        places = category.places.all()  # via related_name
+        serializer = PlaceSerializer(places, many=True, context={'request': request})
+        return Response(serializer.data)
 
 
 class PlaceViewSet(viewsets.ModelViewSet):
